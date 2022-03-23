c80590fac9ea055f717a895e0c21cad9a911d558
vatan780 <vatankumar2018@gmail.com>
2022-03-23 12:08:44 +0530

    frist commit

---
+++ secondproject/secondapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def good_morning_view(request):
+    return HttpResponse('<h1>hello friend good morning !!!</h1>')
+def good_afternon_view(request):
+    return HttpResponse('<h1>hello friend good afternoon !!!</h1>')
+def good_evening_view(request):
+    return HttpResponse('<h1>hello friend good goodevening !!!</h1>')
